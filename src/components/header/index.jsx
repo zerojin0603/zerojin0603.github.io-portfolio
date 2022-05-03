aac6564bf568284cb1a2c759fc1999d9494eeb43
zerojin0603 <44337034+zerojin0603@users.noreply.github.com>
2022-05-03 16:17:29 +0900

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import get from 'lodash/get';
 import React from 'react';
 
-import profileImg from '../../images/logo.png'; 
+import profileImg from '../../images/profile.jpg'; 
 
 const classes = {
   wrapper: 'block mb-6 md:flex',
