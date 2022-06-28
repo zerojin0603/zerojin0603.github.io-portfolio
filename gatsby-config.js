f4dbc719197a696ecb3fd94911effd0751880bb0
zerojin0603 <44337034+zerojin0603@users.noreply.github.com>
2022-06-28 14:42:37 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,12 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Youngjin Kwon',
+    name: '권영진',
     // Main Site Title
-    title: `Youngjin Kwon | Full-Stack Developer`,
+    title: `권영진 | `,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
-    // Optional: Twitter account handle
-    author: `@rfitzio`,
-    // Optional: Github account URL
-    github: `https://github.com/zerojin0603`,
-    // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    description: `끊임없이 도전하며 성장하는 엔지니어입니다.`,
+
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
      education: [
@@ -81,7 +76,7 @@ module.exports = {
     experience: [
       {
         name: 'Nexon Korea',
-        description: 'Security Engineer Internship, October 2021 - April 2022',
+        description: 'Security Engineer, October 2021 - ',
         link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
@@ -108,18 +103,14 @@ module.exports = {
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Languages',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
-      },
-      {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+          'Python, C, Java',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Docker, Amazon Web Services (AWS), ELK',
       },
     ]
   },
